22af88e2e21c14fbf6f87ed4c6d177a46af6a163
Dorian Portet <Dorian.portet159@gmail.com>
2023-12-18 18:06:16 +0100

    Etape 3 : Ajout des bullet points au slider

--- assets/script.js
+++ assets/script.js
@@ -28,3 +28,20 @@ arrowRight.addEventListener("click", () => {
 arrowLeft.addEventListener("click", () => {
     console.log("Vous avez cliqué sur le bouton gauche")
 });
+
+
+// Partie Ajout des bullet points au slider
+
+const bulletDiv = document.querySelector("#banner .dots")
+
+for (let i = 0; i < slides.length; i++) {
+	const bullet = document.createElement("span")
+	bulletDiv.appendChild(bullet)
+	bullet.classList.add("dot")
+    bullet.classList.add(`dot${i}`); // Ajout de classes dot1, dot2, dot3, ...
+	// Ajout de la classe dot_selected au premier élément de la liste
+    if (i === 0) {
+        bullet.classList.add("dot_selected");
+    }
+	console.log(bullet)
+}
